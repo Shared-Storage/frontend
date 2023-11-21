407e866fe70ea96fa3a2af916e4f767c6b72bae4
Rishab Bahal <rishabbahal@gmail.com>
2023-11-20 22:02:54 -0500

    Create item and get Item by organization done

--- src/services/storage.js
+++ src/services/storage.js
@@ -19,6 +19,27 @@ export const createLocation = async (data) => {
   }
 };
 
+export const createItem = async (data) => {
+  try {
+    const response = await axios({
+      method: "post",
+      url: process.env.REACT_APP_STORAGE_SERVICE_BACKEND_LINK + "/item",
+      data: {
+        name: data.name,
+        organization_id: data.organizationId,
+        img: data.img,
+        note: data?.note,
+        location_id: data?.locationId,
+      },
+    });
+    return response;
+  } catch (error) {
+    logger.error(error);
+    throw Error(error.message);
+  }
+};
+
+
 export const getLocationsByOrganization = async (data) => {
   try {
     const response = await axios({
@@ -26,6 +47,22 @@ export const getLocationsByOrganization = async (data) => {
       url:
         process.env.REACT_APP_STORAGE_SERVICE_BACKEND_LINK +
         "/locations/" +
+        data.organizationId
+    });
+    return response;
+  } catch (error) {
+    logger.error(error);
+    throw Error(error.message);
+  }
+};
+
+export const getItemsByOrganization = async (data) => {
+  try {
+    const response = await axios({
+      method: "get",
+      url:
+        process.env.REACT_APP_STORAGE_SERVICE_BACKEND_LINK +
+        "/items/" +
         data.organizationId
     });
     return response;
