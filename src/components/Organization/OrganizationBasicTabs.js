e4bed957658f0f712e2229e8ab626494522b866d
Rishab Bahal <rishabbahal@gmail.com>
2024-02-04 12:13:11 -0500

    Filter item by location working

--- src/components/Organization/OrganizationBasicTabs.js
+++ src/components/Organization/OrganizationBasicTabs.js
@@ -2,7 +2,6 @@ import * as React from "react";
 import PropTypes from "prop-types";
 import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
-import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 
 import LocationTab from "./tabs/LocationTab";
@@ -21,7 +20,7 @@ function OrganizationTabPanel(props) {
     >
       {value === index && (
         <Box sx={{ p: 3 }}>
-          <Typography>{children}</Typography>
+          {children}
         </Box>
       )}
     </div>
